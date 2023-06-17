91a911355c102cdaf0ae352c0a24084dfb8e3977
Suraj Aralihalli <suraj.ara16@gmail.com>
2023-06-17 14:38:30 -0700

    Create semaphore.cpp

---
+++ miscellaneous /semaphore.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+struct semaphore {
+  int count;
+  queue<int> processQueue;
+}
+
+
+void wait(semaphore* sem) {
+  
+  sem->count--; //decrement the count
+  
+  if(sem->count < 0) { //if sem count < 0, add the process to wait queue
+    sem->processQueue.push(curPid);
+  }
+  
+  //call to reschedule
+  
+  return;
+}
+
+
+void signal(semaphore* sem) {
+  
+  sem->count++; //decrement the count
+  
+  if(sem->count <= 0) { //if sem count <= 0, some process is waiting
+    int pid = sem->processQueue.front();
+    sem->processQueue.pop()
+   
+    readyQueue.push(pid); //make the process ready so that it can acquire the semaphore when it becomes ready
+  }
+  
+  return;
+}
